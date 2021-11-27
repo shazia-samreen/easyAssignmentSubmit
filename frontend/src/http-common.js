2dd3029855ac4209bcc4b6da649dc3c8c3650b04
shazia-samreen <arshiyanoureen25@gmail.com>
2021-11-27 15:12:45 +0530

    First version of the easyAssignmentSubmitApplication

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://easy-assignment-submit.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
